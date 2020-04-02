1ccb5b16415e619a05673c463d0b69fc1c63e72e
Marco Carrozzo <ecrrmrc@ericsson.com>
2020-04-02 18:11:23 +0200

    take off

---
+++ quarkus-bench/src/test/java/dev/oz/getting/started/GreetingResourceTest.java
@@ -0,0 +1,21 @@
+package dev.oz.getting.started;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class GreetingResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("localhost:8080")
+          .then()
+             .statusCode(200)
+             .body(is("hello"));
+    }
+
+}
