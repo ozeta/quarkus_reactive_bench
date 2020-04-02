18d128eb2af085fb816484a8d3de63b9e86c2b04
Marco Carrozzo <ozeta86@gmail.com>
2020-04-02 22:02:43 +0000

    initializing env var & creating db stack

--- initialize.sh
+++ initialize.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-set  -euo pipefail
+set -euo pipefail
 
-initialize_gatling () {
-    echo "[INFO] installing gatling from maven repo"
-    curl -O https://repo1.maven.org/maven2/io/gatling/highcharts/gatling-charts-highcharts-bundle/3.3.1/gatling-charts-highcharts-bundle-3.3.1-bundle.zip
-    unzip gatling-charts-highcharts-bundle-3.3.1-bundle.zip
-    rm gatling-charts-highcharts-bundle-3.3.1-bundle.zip
-    mv gatling-charts-highcharts-bundle-3.3.1 gatling-bench
+initialize_gatling() {
+  echo "[INFO] installing gatling from maven repo"
+  curl -sO https://repo1.maven.org/maven2/io/gatling/highcharts/gatling-charts-highcharts-bundle/3.3.1/gatling-charts-highcharts-bundle-3.3.1-bundle.zip
+  unzip -q gatling-charts-highcharts-bundle-3.3.1-bundle.zip
+  rm gatling-charts-highcharts-bundle-3.3.1-bundle.zip
+  rsync -aq gatling-charts-highcharts-bundle-3.3.1 gatling-bench
+  rm gatling-charts-highcharts-bundle-3.3.1-bundle
 }
 
-[[ ! -d gatling-charts-highcharts-bundle-3.3.1/bin ]] && initialize_gatling;
+initialize_env_vars() {
+  echo "[INFO] reading env var from $1"
+  set -a
+  export "$(cat $1 | xargs)"
+  set +a
+}
 
+[[ ! -d gatling-bench/bin ]] && initialize_gatling
+[[ -d env ]] && initialize_env_vars env/local.env
