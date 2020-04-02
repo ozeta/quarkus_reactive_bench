032c3ae691a75ee4123ce5bfbac735df1d9f6f7a
Marco Carrozzo <ozeta86@gmail.com>
2020-04-02 19:49:24 +0200

    moving files and updating readme

--- initialize.sh
+++ initialize.sh
@@ -6,6 +6,7 @@ initialize_gatling () {
     curl -O https://repo1.maven.org/maven2/io/gatling/highcharts/gatling-charts-highcharts-bundle/3.3.1/gatling-charts-highcharts-bundle-3.3.1-bundle.zip
     unzip gatling-charts-highcharts-bundle-3.3.1-bundle.zip
     rm gatling-charts-highcharts-bundle-3.3.1-bundle.zip
+    mv gatling-charts-highcharts-bundle-3.3.1 gatling-bench
 }
 
 [[ ! -d gatling-charts-highcharts-bundle-3.3.1/bin ]] && initialize_gatling;
